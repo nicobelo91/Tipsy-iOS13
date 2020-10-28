b5f0d08713b1345a92f9e45808b06040d9ec54d7
Nico Cobelo <ingcobelo@gmail.com>
2020-10-28 14:00:17 -0400

    Added ResultsViewController file, and wrote code to apply the corresponding tip percentage

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+    }
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        if zeroPctButton.isSelected {
+            print("0.0")
+        } else if tenPctButton.isSelected {
+            print("0.1")
+        } else {
+            print("0.2")
+        }
+    }
+    
+}
+
---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by user183145 on 10/28/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
